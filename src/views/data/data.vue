dbf3961a69a475060ab60eb69a2535651fe8778c
vgoer <hi_goer@163.com>
2023-07-13 20:11:24 +0800

    datav:数据大屏1

--- src/views/data/data.vue
+++ src/views/data/data.vue
@@ -3,6 +3,11 @@
     display: flex;
     justify-content: space-around;
 }
+
+.content{
+    font-size: 28px;
+    text-shadow: 0 0 3px #7acaec;
+}
 </style>
 <!-- 数据面板页面 -->
 <template>
@@ -61,7 +66,7 @@
                 </dv-border-box-13>
             </div>
         </div>
-        <div style="display: flex; justify-content: space-between;">
+        <div style="display: flex; justify-content: space-between; flex-wrap: wrap;">
             <div w50rem h18rem flex justify-center items-center bg-dark>
                 <dv-decoration1 style="width:200px;height:50px;" />
             </div>
@@ -80,6 +85,58 @@
             <div small-bg>
                 <dv-decoration4 :reverse="true" style="width:250px;height:5px;"/>
             </div>
+            <div small-bg>
+                <dv-decoration5 :dur="2" style="width:300px;height:40px;" />
+            </div>
+            <div small-bg>
+                <dv-decoration6 style="width:300px;height:30px;" />
+            </div>
+            <div small-bg>
+                <dv-decoration7 style="width:150px;height:30px;">
+                    <div color-white font-300>
+                        Decoration
+                    </div>
+                </dv-decoration7>
+            </div>
+            <div small-bg>
+                <dv-decoration8 style="width:300px;height:50px;" />
+            </div>
+            <div small-bg>
+                <dv-decoration8 :reverse="true" style="width:300px;height:50px;" />
+            </div>
+            <div small-bg>
+                <dv-decoration-9 style="width:150px;height:150px;">
+                <div color-green font-600 class="content" bg="~ dark/0">
+                    66%
+                </div>
+                </dv-decoration-9>
+            </div>
+            <div small-bg>
+                <dv-decoration-12 style="width:150px;height:150px;" />
+            </div>
+            <div small-bg>
+                <dv-decoration-11 style="width:200px;height:60px;">
+                <div color-green font-600 bg="~ dark/0">
+                    dv-decoration-11
+                </div>
+                </dv-decoration-11>
+            </div>
+        </div>
+        <div>
+            <div small-bg>
+                <dv-decoration-10 style="width:90%;height:5px;" />
+            </div>
+        </div>
+        <div style="display: flex;">
+            <div small-bg>
+                <dv-percent-pond :config="config" style="width:200px;height:100px;" />
+            </div>
+            <div small-bg>
+                <dv-percent-pond :config="config1" style="width:200px;height:100px;" />
+            </div>
+            <div w800px h250px py10 flex justify-center items-center bg-dark>
+                <dv-water-level-pond :config="config2" style="width:200px;height:200px" />
+            </div>
         </div>
     </dv-full-screen-container>
 </template>
@@ -87,7 +144,24 @@
 import { reactive } from 'vue'
 import * as echarts from 'echarts'
 
+const config = reactive({
+    value: 66,
+    borderWidth: 5,
+    borderRadius: 10,
+    borderGap: 5,
+})
 
+
+const config1 = reactive({
+    value: 66,
+    lineDash: [10, 2],
+    localGradient: true,
+})
+
+const config2 = reactive({
+    data: [55],
+    shape: 'round',
+})
 
 
 </script>
