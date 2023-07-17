973d3f71d985cbd3e0a3cfed32684311b44e6ce1
vgoer <hi_goer@163.com>
2023-07-17 15:06:24 +0800

    maps:广东地图

--- src/views/map/map.vue
+++ src/views/map/map.vue
@@ -4,10 +4,18 @@
 </style>
 <!-- 地图 -->
 <template>
-
-
+    <dv-full-screen-container style="height:100vh; background-color: #282c34;">
+        <dv-border-box1  style="z-index: 999;" :color="['#299848', '#fcc422']" backgroundColor="#282c34">
+            <guang></guang>
+        </dv-border-box1>
+    </dv-full-screen-container>
 </template>
 <script setup>
+import { onMounted } from 'vue';
+import guang from './components/guang.vue';
 
+onMounted(() => {
+    
+})
 
 </script>
